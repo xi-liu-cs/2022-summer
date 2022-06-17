7b45759ab62dba2493dd3076e3f65cf95899aeff
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-06-17 15:31:00 -0400

    Update 1.cc

--- cryptography/hw/3.2/1.cc
+++ cryptography/hw/3.2/1.cc
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+using namespace std;
 
 /* kth step: find a quotient q_k, remainder r_k
 r_{k - 2} = q_k * r_{k - 1} + r_k, r_k = r_{k - 2} % r_{k - 1} 
@@ -21,7 +25,67 @@ int euclid_gcd(int a, int b)
     return a > -a ? a : -a;
 }
 
+/* (left1, left2) = (right1, right2) */
+#define parallel_assign(left1, left2, right1, right2)\
+{\
+    int tmp = right1;\
+    left2 = right2;\
+    left1 = tmp;\
+}
+
+/* return x and y s.t. ax + by = gcd(a, b) 
+r is remainder 
+return value: (*ret, ret[1]) = (x, y)
+ret[2] = gcd(a, b) */
+int * extend_euclid_gcd(int a, int b)
+{
+    int old_r = a, r = b,
+    old_s = 1, s = 0,
+    old_t = 0, t = 1;
+    while(r)
+    {
+        int quotient = old_r / r;
+        parallel_assign(old_r, r, r, old_r - quotient * r);
+        parallel_assign(old_s, s, s, old_s - quotient * s);
+        parallel_assign(old_t, t, t, old_t - quotient * t);
+    }
+    int * ret = (int *)malloc(3 * sizeof(int));
+    *ret = old_s, ret[1] = old_t, ret[2] = old_r;
+    return ret;
+}
+
 int main()
 {
-    printf("%d\n", euclid_gcd(3, 6));
+    int p = 8,
+    q = 9,
+    n = p * q,
+    _n = (p - 1) * (q - 1);
+    
+    vector<int> key;
+    for(int i = 2; i < _n; ++i)
+    {
+        int gcd = euclid_gcd(_n, i);
+        if(gcd == 1)
+            key.push_back(i);
+    }
+    
+    int enc = 3, dec;
+    int * ext_gcd = extend_euclid_gcd(_n, enc);
+    if(ext_gcd[2] == 1)
+    {
+        dec = ext_gcd[1];
+        printf("decryption key is %d\n", dec);
+    }
+    else
+        printf("no multiplicative inverse for given encryption key\n");
+    int msg = 2, /* message */
+    sig = (int)pow(msg, dec) % n, /* signature */
+    msg_gen = (int)pow(sig, enc) % n; /* generated message */
+    printf("sig = %d\n", sig);
+    printf("n = %d\n", n);
+    printf("msg_gen = %d\n", msg_gen);
+    if(msg == msg_gen)
+        printf("accept\n");
+    else
+        printf("reject\n");
 }
