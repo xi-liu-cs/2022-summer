ca928d4845a59a4d27c5a42dd323171b35bbdc20
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-07-10 17:34:05 -0400

    Update msg.sql

--- cryptography/hw/6/msg.sql
+++ cryptography/hw/6/msg.sql
@@ -3,6 +3,6 @@ create table tb1
 	usr int,
 	msg text
 );
-insert into tb1 values(1, 'hi');
-insert into tb1 values(0, 'bye');
+insert into tb1 values(0, 'hi');
+insert into tb1 values(1, 'bye');
 select * from tb1;
