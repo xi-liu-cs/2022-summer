ccf1908aec1444c811cbcf8a973d98d5fd487d3e
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-07-24 19:41:18 -0400

    Update 0.js

--- graphics/a/2022.7.24/0.js
+++ graphics/a/2022.7.24/0.js
@@ -19,18 +19,17 @@ function main()
   vertex_shader = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertex_shader, vertex);
   gl.compileShader(vertex_shader);
-  
   fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
   gl.shaderSource(fragment_shader, fragment);
   gl.compileShader(fragment_shader);
-  
   program = gl.createProgram();
   gl.attachShader(program, vertex_shader);
   gl.attachShader(program, fragment_shader);
   gl.linkProgram(program);
   gl.useProgram(program);
+  gl.program = program;
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0); /* specify the color for clearing <canvas> */
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);   /* specify the color for clearing <canvas> */
   gl.clear(gl.COLOR_BUFFER_BIT); /* clear canvas */
   gl.drawArrays(gl.POINTS, 0, 1); /* draw a point */
 }
