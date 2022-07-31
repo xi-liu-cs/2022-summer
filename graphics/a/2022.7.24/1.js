dcd5f5e8c2bb174412a9b247bc3ba1df01ce8cb4
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-07-31 13:54:11 -0400

    Update 1.js

--- graphics/a/2022.7.24/1.js
+++ graphics/a/2022.7.24/1.js
@@ -1,9 +1,10 @@
 var vertex = 
-`attribute vec4 a_pos;
+`attribute vec4 a_Position;
+attribute float a_PointSize;
 void main() 
 { 
-    gl_Position = a_pos;
-    gl_PointSize = 10.0;
+    gl_Position = a_Position;
+    gl_PointSize = a_PointSize;
 }`; 
 
 var fragment = 
@@ -14,12 +15,15 @@ var fragment =
 
 function main() 
 {
-  var canvas = document.getElementById('1');
-  var gl = canvas.getContext('webgl');
+  canvas = document.getElementById('1');
+  gl = canvas.getContext('webgl');
   shader_init(gl, vertex, fragment);
 
-  var a_pos = gl.getAttribLocation(gl.program, 'a_pos'); 
-  gl.vertexAttrib3f(a_pos, 0.0, 0.0, 0.0);
+  a_Position = gl.getAttribLocation(gl.program, 'a_Position'); 
+  a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+  gl.vertexAttrib1f(a_PointSize, 10.0);
+  
   gl.clearColor(0.0, 0.0, 0.0, 1.0); 
   gl.clear(gl.COLOR_BUFFER_BIT); 
   gl.drawArrays(gl.POINTS, 0, 1);
