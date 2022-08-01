3d425716e78fd4567e5dde7692c5812cca3d17a9
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-08-01 15:49:41 -0400

    Update 0.js

--- graphics/a/2022.7.24/0.js
+++ graphics/a/2022.7.24/0.js
@@ -1,11 +1,11 @@
-var vertex = 
+vertex = 
 `void main()
 {
   gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
   gl_PointSize = 10.0;                    
 }`;
 
-var fragment =
+fragment =
 `void main() 
 {
   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); 
